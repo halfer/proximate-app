1488afa6335b54e88ce463bf96894deb6d09f42c
Jon Hinks <projects.ws@jondh.me.uk>
2017-03-20 15:50:04 +0000

    Add demo of fetching data from API

--- public/index.php
+++ public/index.php
@@ -1,1 +1,15 @@
-Hello
+<?php
+
+$root = realpath(__DIR__ . '/..');
+require_once $root . '/vendor/autoload.php';
+
+$app = new \Slim\App();
+
+// Url browser
+$app->get('/', function ($request, $response) {
+
+    $curl = new PestJSON("http://proximate-api:8080/");
+    $data = $curl->get('/count');
+    print_r($data);
+});
+$app->run();
